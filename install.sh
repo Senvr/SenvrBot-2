a0b00ef01c86dc761d031ea22b6733dbb0329c04
Lee Ikard <Leeeyecard@gmail.com>
2018-11-04 15:38:21 -0800

    update 2
    LET ME CLARIFY SOME THINGS
    I haven't updated in a while.
    This is a total clusterfuck. Optimization and "graphical" edits are to come. Other than that, it works and I don't care. This was a nightmare for me, i don't know why.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-set -e
+echo "DONT RUN AS SUDO (please)"
 #required crap
 sudo apt install libssl-dev  -y
 sudo apt install python3 python3-pip libffi-dev -y
@@ -9,11 +9,21 @@ if [ $1 == "audio" ]; then
 	sudo apt install libopus0 ffmpeg
 	sudo python3 -m pip install -U discord.py[voice] 
 fi
-
+sudo apt install python-mysqldb mysql-server python-mysql.connector
 sudo python3 -m pip install -U discord.py
-
+sudo pip3 install mysql-connector-python-rf
 #extra features: 
 sudo pip3 install tinytag
+
+#configure mysql
+clear
+echo "configuring MYSQL under $USER (ctrl C if already done)"
+sleep 2
+sudo mysql_secure_installation
+#echo "making database and user... (ctrl c if already done)" 
+#sleep 2
+#printf "creating user SenvrBot2@localhost identified by (this is a password): "
+#read identity -s
 
 #done
 echo "Done."
--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-import discord, asyncio, os, re, datetime, random
+import discord, asyncio, os, re, datetime, random, datetime, string
+from pathlib import Path
+import mysql.connector
+from discord.utils import get
 from discord.ext import commands
+import hashlib
 from discord.ext.commands import Bot
-prefix = "$"
+prefix = "%"
 
 bot = commands.Bot(command_prefix=prefix)
-TOKEN="NTA4MjA1MTQ5MTQ4MjgyODkx.Dr72hQ.Pg6XyqE3FDqCC1Olhfbxc8V7wKQ"
+TOKEN="REPLACEME"
+
+def readFileVar(varName):
+	if Path(var).is_file():
+		v=open(var,"r")
+		print(var);
+		v.close
+		return var;
+	else:
+		print("ERROR: "+var+" unset!");
+		return None;
+def writeFileVar(var, varcontent):
+	v=open(var,"w")
+	v.write(str(varcontent))
+	v.close
+	print(var)
+	return None;
+def titleToId(title):
+	ID=re.sub('[abcdefghijklmnopqrstuvwxyz]', '',str(hashlib.sha224(title.encode('utf-8')).hexdigest())).strip()
+	return ID;
 @bot.event
 async def on_ready():
 	print('------')
@@ -19,6 +42,12 @@ async def on_ready():
 	print("pid="+str(os.getpid()))
 
 	print('------')
+@bot.command(pass_context=True)
+async def say(ctx, repeats):
+	while int(repeats) > 0:
+		repeats=int(repeats) - 1
+		bot.say(str(ctx.message.content))
+	
 @bot.command()
 async def test():
 	await bot.say(bot.user.name+" is alive!")
@@ -31,5 +60,39 @@ async def github():
 async def on_command_error(error, ctx):
 	await bot.send_message(ctx.message.channel, "`"+str(error)+"`")
 	print(error)
+@bot.command(pass_context=True)
+async def quickpoll(ctx, votetitle, time):
+	if votetitle == "":
+		bot.say("You need to input a votetitle.")
+		return
+	if time == "":
+		bot.say("You need to input a time.")
+		return
+	message=ctx.message.content
+	channel = ctx.message.channel
+	choices = {"🇦": "Approve",
+				"🇧": "Deny",
+				"🇨": "Abstain"}
+
+	vote = discord.Embed(title="**[POLL]**", description=" ", color=0x00ff00)
+	value = "\n".join("- {} {}".format(*item) for item in choices.items()) 
+	vote.add_field(name=votetitle, value=value, inline=True)
+
+	message_1 = await bot.send_message(channel, embed=vote)
+
+	for choice in choices:
+		await bot.add_reaction(message_1, choice)
+
+	await asyncio.sleep(int(time))
+	message_1 = await bot.get_message(channel, message_1.id)
+
+
+	counts = {react.emoji: react.count for react in message_1.reactions}
+	winner = max(choices, key=counts.get)
+
+	await bot.send_message(channel, "Option '{}' has won!".format(choices[winner]))
+	#await ctx.message.clear_reactions
+def setup(bot):
+    bot.add_cog(QuickPoll(bot))
 
 bot.run(TOKEN)
